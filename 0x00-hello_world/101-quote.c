7feaf5e61fdd2e30386c8f695b170a086f3d9d87
root <root@DUSABE.localdomain>
2023-02-17 00:23:41 +0200

    print fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - print to standard error
  * Return 1
  */
-int main(void)
+int main()
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }

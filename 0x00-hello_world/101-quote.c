e9c8940822261c813116288bfc2e41d3d0153a66
root <root@DUSABE.localdomain>
2023-02-17 00:26:08 +0200

    void fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - print to standard error
  * Return 1
  */
-int main()
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }

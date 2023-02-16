0b709630dc37066204e00222212bdcecf1bd8412
root <root@DUSABE.localdomain>
2023-02-17 00:43:19 +0200

    fix main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print to standard error
- * Return 1
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5i9);
 return (1);
 }
